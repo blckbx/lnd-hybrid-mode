e977524e71a84278d219de61e9683dcb5e18fe6b
osito <74455114+blckbx@users.noreply.github.com>
2021-12-23 17:17:18 +0100

    Create update-ddns.sh

---
+++ update-ddns.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# for some DDNS providers there is the possibility to trigger an IP update manually
+# this script checks the current IP and compares it to the last one fetched
+# if it differs, new IP is saved and Update-URL is triggered. DDNS provider gets new IP.
+# usage: set script in cronjob every x hours
+
+FILE=$PWD/ip.txt
+UPDATEURL=<UPDATE-URL>
+
+newip=$(curl -s https://api.ipify.org)
+#echo newip=$newip
+
+if [ -f "$FILE" ]; then 
+    echo "null" > $FILE
+fi
+
+ip=$(cat $FILE)
+
+if [[ "$ip" != "$newip" ]]; then
+   echo $newip > $FILE
+   echo "IP change detected"
+   curl -s $UPDATEURL
+else
+   echo "No IP change detected"
+fi
