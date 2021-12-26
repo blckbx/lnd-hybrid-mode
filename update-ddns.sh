da8336d089e7d5f4fd2899ad515cb03960d3b7fe
osito <74455114+blckbx@users.noreply.github.com>
2021-12-26 13:19:12 +0100

    Update update-ddns.sh

--- update-ddns.sh
+++ update-ddns.sh
@@ -5,14 +5,16 @@
 # If it differs, new IP is saved and update-URL is triggered. DDNS provider fetches new IP.
 # usage: setup script as cronjob every x hours
 
+# local file to save and compare fetched external IP
 FILE=$PWD/ip.txt
+# Update-URL of DDNS provider, e.g.: https://<yourdnsprovider.com>/key=<accountsecret>&host=<yourdomain> (pls check readme/faq of your provider)
 UPDATEURL=<UPDATE-URL>
 
 newip=$(curl -s https://api.ipify.org)
 
-# if [[ ! -f "$FILE" ]]; then 
-#    echo "null" > $FILE
-# fi
+if [[ ! -f "$FILE" ]]; then
+    echo "null" > $FILE
+fi
 
 ip=$(cat $FILE)
 
